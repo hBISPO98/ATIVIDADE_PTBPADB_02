3e5c98721846e14ec7746a22b8437920f0dff197
hBISPO98 <b.hiandra@aluno.ifsp.edu.br>
2026-03-09 07:40:00 -0300

    Update contagem_registros.sql

--- contagem_registros.sql
+++ contagem_registros.sql
@@ -1,34 +1,35 @@
--- PTBPADB - Item 5: Contagem de registros por tabela
+-- Script de contagem de registros por tabela
+-- PTBPADB - Item 5
 
--- Tabela: advisor
-SELECT 'advisor' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[advisor];
-
--- Tabela: classroom
-SELECT 'classroom' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[classroom];
-
--- Tabela: course
-SELECT 'course' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[course];
-
--- Tabela: department
-SELECT 'department' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[department];
-
--- Tabela: instructor
-SELECT 'instructor' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[instructor];
-
--- Tabela: prereq
-SELECT 'prereq' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[prereq];
-
--- Tabela: section
-SELECT 'section' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[section];
-
--- Tabela: student
-SELECT 'student' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[student];
-
--- Tabela: takes
-SELECT 'takes' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[takes];
-
--- Tabela: teaches
-SELECT 'teaches' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[teaches];
-
--- Tabela: time_slot
-SELECT 'time_slot' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[time_slot];
+-- 1. Contagem da tabela advisor
+SELECT 'advisor' AS Tabela, COUNT(*) AS Quantidade FROM [dbo].[advisor]
+UNION ALL
+-- 2. Contagem da tabela classroom
+SELECT 'classroom', COUNT(*) FROM [dbo].[classroom]
+UNION ALL
+-- 3. Contagem da tabela course
+SELECT 'course', COUNT(*) FROM [dbo].[course]
+UNION ALL
+-- 4. Contagem da tabela department
+SELECT 'department', COUNT(*) FROM [dbo].[department]
+UNION ALL
+-- 5. Contagem da tabela instructor
+SELECT 'instructor', COUNT(*) FROM [dbo].[instructor]
+UNION ALL
+-- 6. Contagem da tabela prereq
+SELECT 'prereq', COUNT(*) FROM [dbo].[prereq]
+UNION ALL
+-- 7. Contagem da tabela section
+SELECT 'section', COUNT(*) FROM [dbo].[section]
+UNION ALL
+-- 8. Contagem da tabela student
+SELECT 'student', COUNT(*) FROM [dbo].[student]
+UNION ALL
+-- 9. Contagem da tabela takes
+SELECT 'takes', COUNT(*) FROM [dbo].[takes]
+UNION ALL
+-- 10. Contagem da tabela teaches
+SELECT 'teaches', COUNT(*) FROM [dbo].[teaches]
+UNION ALL
+-- 11. Contagem da tabela time_slot
+SELECT 'time_slot', COUNT(*) FROM [dbo].[time_slot];
